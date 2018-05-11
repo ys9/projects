67babf987f88ef95362dee5f40b5c14186d454b6
Brendan Wilson <brendan.wilson@student.nmt.edu>
2018-05-11 12:17:46 -0600

    Delete ssh.h

--- TuxSSH/src/ssh.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int checkProfiles(int profileExists, char *uname, char *hostname);
-int config(int callKeygen);
-void startup(char *username, char *hostname, char *password, int profileExists);
-void sshLogin(char *uname, char *hostname);
-void scpFrom(char *uname, char *hostname);
-void scpTo(char *uname, char *hostname);
