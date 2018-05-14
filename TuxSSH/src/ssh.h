6edd43bafa12fe44c00090585e534a66ea5f955d
Brendan Wilson <brendan.wilson@student.nmt.edu>
2018-05-14 16:26:34 -0600

    Update ssh.h

--- TuxSSH/src/ssh.h
+++ TuxSSH/src/ssh.h
@@ -2,7 +2,7 @@
 
 int checkProfiles(int profileExists, char *uname, char *hostname);
 int config(int callKeygen);
-void startup(char *username, char *hostname, char *password, int profileExists);
+void startup(char *uname, char *hostname, char *password, int profileExists);
 void sshLogin(char *uname, char *hostname);
 void scpFrom(char *uname, char *hostname);
 void scpTo(char *uname, char *hostname);
