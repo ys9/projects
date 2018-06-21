405949a8f5155043650113d0150c34b6af887e6f
Brendan Wilson <brendan.wilson@student.nmt.edu>
2018-06-21 10:41:14 -0600

    Add files via upload

---
+++ GTKCalculator/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+rm /usr/share/applications/simple_calculator.desktop
+rm /usr/bin/simpleCalc
+rm /usr/share/pixmaps/calc.ico
+rm uninstall.sh
